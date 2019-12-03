8ffd57dc0859c3a7207fb6d76b7c9e78c29cf79e
Tounoussi Khalid <44621176+tounoussiKhalid@users.noreply.github.com>
2019-12-03 18:03:54 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar.js";
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Articles from "./pages/Articles"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
 import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
-  state={
-    page: 'home'
-  }
+  state = {
+    page: "home"
+  };
 
-  handleNavClicks = (e) =>{
-      const name = e.target.getAttribute("name");
-      const newState = {
-        page : name
-      };
-
-      this.setState( newState );
-
-  }
-
-  render(){
+  render() {
     return (
-    <div className="App">
-      <Navbar handleNavClicks= { (e) => this.handleNavClicks(e) }/>
+      <Router>
+        <Navbar />
 
-      {this.state.page ==="home" ? <Home /> : 
-      this.state.page ==="about" ? <About /> :
-      <Articles />}
-    </div>
-  );
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/articles">
+            <Articles />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import App from './App.js';
-
+import App from "./App.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);

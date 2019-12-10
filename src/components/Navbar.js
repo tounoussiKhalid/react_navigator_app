80e6485179404e19d998ef9d05fb8d4c61fce8a4
Client15@EST.LAN <Client15@EST.LAN>
2019-12-10 09:43:49 +0000

    set up navigator

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
       <nav class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item active">Home</li>
+            <li class="nav-item active">
+                <Link to ="/"> Home</Link>
+              </li>
             <li class="nav-item">
               <Link to="/about">About</Link>
             </li>
